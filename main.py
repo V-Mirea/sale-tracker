f1f22796a7feb9e2bb8877c6b9f36dc3481af32f
SpeedOfSmell <vldmirea@gmail.com>
2018-04-17 19:19:19 -0400

    Started project

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import pymongo
-from steam import SteamClient
+#from steam import SteamClient
 
 from tkinter import *
 from tkinter import ttk
 
 def createGui():
+    # Main window
     root = Tk()
-    root.title("Sale Tracker")
+    root.title = "Sale Tracker"  
 
+    # Frame containing widgets to add a new game
+    content = ttk.Frame(root, padding=(10, 5), borderwidth=5, relief="sunken", width=400, height=250)
+    content.grid(column=0, row=0)
+
+    addFrame = ttk.Frame(content, borderwidth=5, relief="sunken")
+    content.grid(column=0, row=0)
+
+    label = ttk.Label(addFrame, text="heelo")
+    label.grid(column=1, row = 0)
+
+    listFrame = ttk.Frame(content, borderwidth=5, relief="sunken")
+    content.grid(column=1, row=0)
+
+    root.mainloop()
 
 def main():
     username = "admin"
@@ -16,12 +31,7 @@ def main():
     client = pymongo.MongoClient("mongodb+srv://" + username + ":" + password + "@cluster0-kaauu.mongodb.net")
     db = client['sale-tracker']
 
-    #createGui()
-
-    cli = SteamClient()
-
-    session = cli.get_web_session()  # returns requests.Session
-    session.get('https://store.steampowered.com')
+    createGui()
 
 if __name__ == "__main__":
     main()
