0548a533d9e19964d64b0ca29c77a589a5dfb171
SpeedOfSmell <vldmirea@gmail.com>
2018-04-21 17:01:36 -0400

    dont feel like typing a message

--- README_VENV.txt
+++ README_VENV.txt
@@ -1,33 +1,25 @@
-The purpose is to create a development enviroment that is consistent for everyone (just the both of us). So that we both know which library
-each of us have, so that we won't get compiler error when importing moduels, etc... Update the pip install list so that I know which library
-you've downloaded so that I can download it for myself as well.
 
 pip install in virtual enviroment:
 ----------------------------------
 pip install pymongo 
 pip install dnspython
 
-For Tensorflow and Setting Up Virtual Enviroment:
+pip install BeautifulSoup4
+
+For Setting Up Virtual Enviroment:
 ------------------------------------------------
-1. Install Python 3.5 64bit version (must be 64bit version).
-https://www.python.org/downloads/release/python-352/
 
-2. cmd: py -3.5 -m pip install virtualenv
+1. navigate to project folder
 
-3. navigate to project folder
-
-4. cmd: py -3.5 virtualenv venv
-looks something like this: ..\Artificial-Intelligence-Hand-Writing> py -3.5 virtualenv venv
+2. cmd: virtualenv [name]
 
 How To Use Virtual Environment:
 -------------------------------
 1. Go to project folder
-looks something like this: ..\Artificial-Intelligence-Hand-Writing>
 
-2. Type 'venv\Scripts\activate
-looks something like this: ..\Artificial-Intelligence-Hand-Writing>venv\Scripts\activate
+2. Type '[name]\Scripts\activate
 
-3. should look something like: (venv) C:\...
+3. should look something like: ([name]) C:\...
 
 4. start developing in virtual enviroment type 'deactivate' to simply deactivate.
 
--- main.py
+++ main.py
@@ -1,89 +1,121 @@
+import datetime
+
 import pymongo
-#from steam import SteamClient
 
 from tkinter import *
 from tkinter import ttk
 
-def createGui():
-    # Main window
-    root = Tk()
-    root.title = "Sale Tracker"  
-
-    # Frame containing widgets to add a new game
-    content = ttk.Frame(root, padding=(10, 5), width=400, height=250)
-    content.grid(column=0, row=0, sticky = (N, S, E, W))
-
-    addFrame = ttk.Frame(content, borderwidth=5, relief=SOLID)
-    addFrame.grid(column=0, row=0)
-    # ---v
-    label1 = ttk.Label(addFrame, text="Title:")
-    label1.grid(column=0, row = 0, sticky=W)   
-
-    label2 = ttk.Label(addFrame, text="Stores:")
-    label2.grid(column=1, row = 1)
-    
-    title = StringVar()
-    txtTitle = ttk.Entry(addFrame, textvariable=title)
-    txtTitle.grid(column=0, row=1, sticky=NW)
-
-    storesFrame = ttk.Frame(addFrame, borderwidth=5, relief="solid")
-    storesFrame.grid(column=1, row=2, rowspan=3)
-    #---v
-    checkSteam = StringVar()
-    chkSteam = ttk.Checkbutton(storesFrame, text="Steam", variable=checkSteam)
-    chkSteam.grid(column=0, row=0)
-
-    checkOrigin = StringVar()
-    chkOrigin = ttk.Checkbutton(storesFrame, text="Origin", variable=checkOrigin)
-    chkOrigin.grid(column=0, row=1)
-    # ----
-
-    label3 = ttk.Label(addFrame, text="Price:")
-    label3.grid(column=0, row = 2, sticky=W)   
-
-    price = StringVar()
-    txtPrice = ttk.Entry(addFrame, textvariable=price)
-    txtPrice.grid(column=0, row=3)
-
-    label4 = ttk.Label(addFrame, text="Expiration:")
-    label4.grid(column=0, row = 4, sticky=W)
-
-    expiration = StringVar()
-    txtExp = ttk.Entry(addFrame, textvariable=expiration)
-    txtExp.grid(column=0, row=5)
-
-    btnAdd = ttk.Button(addFrame, text='Add', command=addGame)
-    btnAdd.grid(column=0, row=6, columnspan=2)
-    #-----------------------------------------------------
-
-    listFrame = ttk.Frame(content, borderwidth=5, relief=SOLID)
-    listFrame.grid(column=1, row=0, sticky=(N, S))
-
-    gamesFrame = ttk.Frame(listFrame, borderwidth=5, relief="sunken", padding = 10)
-    gamesFrame.grid(column=0, row=0)
-
-    label4 = ttk.Label(gamesFrame, text="Fallout 4")
-    label4.grid(column=0, row = 0) 
-
-    label5 = ttk.Label(gamesFrame, text="Nier: Automata")
-    label5.grid(column=0, row = 1) 
-
-    btnDelete = ttk.Button(listFrame, text='Delete', command=addGame)
-    btnDelete.grid(column=0, row=1)
-
-    root.mainloop()
+from urllib.request import urlopen
+from bs4 import BeautifulSoup  
 
 def addGame():
-    pass
+    lstGames.insert(END, txtTitle.get())
 
-def main():
-    username = "admin"
-    password = "passwordd"
+    strDate = txtExp.get().split("/")
+    date = datetime.datetime(int(strDate[2]), int(strDate[0]), int(strDate[1]))
 
-    client = pymongo.MongoClient("mongodb+srv://" + username + ":" + password + "@cluster0-kaauu.mongodb.net")
-    db = client['sale-tracker']
+    entry = {"title": txtTitle.get(),
+             "price": int(txtPrice.get()),
+             "expiration": date}
 
-    createGui()
+    db["games"].insert_one(entry)
 
-if __name__ == "__main__":
-    main()
+def deleteGame():
+    lstGames.delete(lstGames.curselection())
+
+def scrape():
+    term = txtTitle.get().replace(' ', '+')
+    url = "http://store.steampowered.com/search/?term=" + term
+
+    page = urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+
+    price = soup.find("div", {"class": "search_price"})
+    if(price.span): # If there's a sale, the original price will be in a span so get rid of it
+        price.span.extract()       
+    price = price.text.strip()
+      
+    print(price)  
+
+username = "admin"
+password = "passwordd"
+    
+client = pymongo.MongoClient("mongodb+srv://" + username + ":" + password + "@cluster0-kaauu.mongodb.net")
+db = client['sale-tracker']
+
+# Main window
+root = Tk()
+root.title = "Sale Tracker"  
+
+# Frame containing widgets to add a new game
+content = ttk.Frame(root, padding=(10, 5), width=400, height=250)
+content.grid(column=0, row=0, sticky = (N, S, E, W))
+
+addFrame = ttk.Frame(content, borderwidth=5, relief=SOLID)
+addFrame.grid(column=0, row=0, sticky = (N, S, E, W))
+# ---v
+label1 = ttk.Label(addFrame, text="Title:")
+label1.grid(column=0, row = 0, sticky=W)   
+
+label2 = ttk.Label(addFrame, text="Stores:")
+label2.grid(column=1, row = 1)
+    
+title = StringVar()
+txtTitle = ttk.Entry(addFrame, textvariable=title)
+txtTitle.grid(column=0, row=1, sticky=NW)
+
+storesFrame = ttk.Frame(addFrame, borderwidth=5, relief="solid")
+storesFrame.grid(column=1, row=2, rowspan=3)
+#---v
+checkSteam = StringVar()
+chkSteam = ttk.Checkbutton(storesFrame, text="Steam", variable=checkSteam)
+chkSteam.grid(column=0, row=0)
+
+checkOrigin = StringVar()
+chkOrigin = ttk.Checkbutton(storesFrame, text="Origin", variable=checkOrigin)
+chkOrigin.grid(column=0, row=1)
+# ----
+
+label3 = ttk.Label(addFrame, text="Price:")
+label3.grid(column=0, row = 2, sticky=W)   
+
+price = StringVar()
+txtPrice = ttk.Entry(addFrame, textvariable=price)
+txtPrice.grid(column=0, row=3)
+
+label4 = ttk.Label(addFrame, text="Expiration:")
+label4.grid(column=0, row = 4, sticky=W)
+
+expiration = StringVar()
+txtExp = ttk.Entry(addFrame, textvariable=expiration)
+txtExp.grid(column=0, row=5)
+
+btnAdd = ttk.Button(addFrame, text='Add', command=addGame)
+btnAdd.grid(column=0, row=6, columnspan=2)
+#-----------------------------------------------------
+
+listFrame = ttk.Frame(content, borderwidth=5, relief=SOLID)
+listFrame.grid(column=1, row=0, sticky=(N, S))
+
+gamesScrollbar = Scrollbar(listFrame)
+gamesScrollbar.grid(column=1, row=0, sticky=(N, S))
+
+lstGames = Listbox(listFrame)
+lstGames.grid(column=0, row=0)
+
+lstGames.config(yscrollcommand=gamesScrollbar.set)
+gamesScrollbar.config(command=lstGames.yview)
+
+#gamesFrame = ttk.Frame(listFrame, borderwidth=5, relief="sunken", padding = 10)
+#gamesFrame.grid(column=0, row=0)
+
+#label4 = ttk.Label(gamesFrame, text="Fallout 4")
+#label4.grid(column=0, row = 0) 
+
+#label5 = ttk.Label(gamesFrame, text="Nier: Automata")
+#label5.grid(column=0, row = 1) 
+
+btnDelete = ttk.Button(listFrame, text='Delete', command=deleteGame)
+btnDelete.grid(column=0, row=1)
+
+root.mainloop()
