6aea45694b055d3d3f2cfd3ab385d4c130a4f2d0
SpeedOfSmell <vldmirea@gmail.com>
2018-04-24 19:53:43 -0400

    Refining add function

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
 import datetime
+import decimal
+from decimal import Decimal
 
 import pymongo
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 from urllib.request import urlopen
-from bs4 import BeautifulSoup  
+from bs4 import BeautifulSoup
+
+from uuid import getnode as getMac
+
+def clearForm():
+    for txtbox in filter(lambda w:isinstance(w, Entry), addFrame.children.values()):
+        txtbox.delete(0, END)
 
 def addGame():
-    lstGames.insert(END, txtTitle.get())
+    title = txtTitle.get()
 
-    strDate = txtExp.get().split("/")
-    date = datetime.datetime(int(strDate[2]), int(strDate[0]), int(strDate[1]))
+    #strDate = txtExp.get().split("/")
+    #date = datetime.datetime(int(strDate[2]), int(strDate[0]), int(strDate[1]))
 
-    entry = {"title": txtTitle.get(),
-             "price": int(txtPrice.get()),
-             "expiration": date}
+    entry = {"title": title}
 
-    db["games"].insert_one(entry)
+    try:
+        amount = Decimal(txtPrice.get())
+    except (ValueError, decimal.InvalidOperation):
+        amount = getPrice(title, False)[0] - Decimal("0.01") # If no/bad price supplied, set limit to just below current regular price
+
+    price = getPrice(title, True)
+    if (amount > price[0]): # Notify user if the game is already cheaper than the price they set
+        messagebox.showinfo("It's your lucky day!", title + " is already on sale for " + price[1] + str(price[0]))
+    else:
+        entry.update({"price": float(amount)})
+
+        try:
+            strDate = txtExp.get().split("/")
+            date = datetime.datetime(int(strDate[2]), int(strDate[0]), int(strDate[1]))
+
+            entry.update({"expiration": date})
+        except (ValueError, IndexError):
+            pass
+
+        db["games"].insert_one(entry)
+        lstGames.insert(END, title)
+
+    clearForm()
 
 def deleteGame():
+    db["games"].find_one_and_delete({"title": lstGames.get(lstGames.curselection())})
     lstGames.delete(lstGames.curselection())
 
-def scrape():
+# Takes string with game name and optional boolean for price to get
+# Returns price of given game and a character representing currency. Regular price or sale price depending on second argument
+def getPrice(game, sale = True):
     term = txtTitle.get().replace(' ', '+')
     url = "http://store.steampowered.com/search/?term=" + term
 
@@ -31,11 +63,20 @@ def scrape():
     soup = BeautifulSoup(page, 'html.parser')
 
     price = soup.find("div", {"class": "search_price"})
-    if(price.span): # If there's a sale, the original price will be in a span so get rid of it
-        price.span.extract()       
+    if(price.strike):
+        if(sale == True): # If there's a sale, the original price will be striked so look for that and get rid of it
+            price.strike.extract()
+        elif(sale == False):
+            price = price.strike
+
     price = price.text.strip()
       
-    print(price)  
+    return Decimal(price[1:]), price[0] # Remove currency sign, convert to number, and return, along with currency
+
+def populateGamesList():
+    games = db["games"].find()
+    for game in games:
+        lstGames.insert(END, game["title"])
 
 username = "admin"
 password = "passwordd"
@@ -56,38 +97,38 @@ addFrame.grid(column=0, row=0, sticky = (N, S, E, W))
 # ---v
 label1 = ttk.Label(addFrame, text="Title:")
 label1.grid(column=0, row = 0, sticky=W)   
-
-label2 = ttk.Label(addFrame, text="Stores:")
-label2.grid(column=1, row = 1)
-    
-title = StringVar()
-txtTitle = ttk.Entry(addFrame, textvariable=title)
+   
+#title = StringVar()
+txtTitle = ttk.Entry(addFrame)
 txtTitle.grid(column=0, row=1, sticky=NW)
 
-storesFrame = ttk.Frame(addFrame, borderwidth=5, relief="solid")
-storesFrame.grid(column=1, row=2, rowspan=3)
-#---v
-checkSteam = StringVar()
-chkSteam = ttk.Checkbutton(storesFrame, text="Steam", variable=checkSteam)
-chkSteam.grid(column=0, row=0)
+#label2 = ttk.Label(addFrame, text="Stores:")
+#label2.grid(column=1, row = 1)
+#
+#storesFrame = ttk.Frame(addFrame, borderwidth=5, relief="solid")
+#storesFrame.grid(column=1, row=2, rowspan=3)
+##---v
+##checkSteam = StringVar()
+#chkSteam = ttk.Checkbutton(storesFrame, text="Steam")
+#chkSteam.grid(column=0, row=0)
 
-checkOrigin = StringVar()
-chkOrigin = ttk.Checkbutton(storesFrame, text="Origin", variable=checkOrigin)
-chkOrigin.grid(column=0, row=1)
-# ----
+##checkOrigin = StringVar()
+#chkOrigin = ttk.Checkbutton(storesFrame, text="Origin")
+#chkOrigin.grid(column=0, row=1)
+## ----
 
 label3 = ttk.Label(addFrame, text="Price:")
 label3.grid(column=0, row = 2, sticky=W)   
 
-price = StringVar()
-txtPrice = ttk.Entry(addFrame, textvariable=price)
+#price = StringVar()
+txtPrice = ttk.Entry(addFrame)
 txtPrice.grid(column=0, row=3)
 
 label4 = ttk.Label(addFrame, text="Expiration:")
 label4.grid(column=0, row = 4, sticky=W)
 
-expiration = StringVar()
-txtExp = ttk.Entry(addFrame, textvariable=expiration)
+#expiration = StringVar()
+txtExp = ttk.Entry(addFrame)
 txtExp.grid(column=0, row=5)
 
 btnAdd = ttk.Button(addFrame, text='Add', command=addGame)
@@ -103,19 +144,14 @@ gamesScrollbar.grid(column=1, row=0, sticky=(N, S))
 lstGames = Listbox(listFrame)
 lstGames.grid(column=0, row=0)
 
+populateGamesList()
+
 lstGames.config(yscrollcommand=gamesScrollbar.set)
 gamesScrollbar.config(command=lstGames.yview)
 
-#gamesFrame = ttk.Frame(listFrame, borderwidth=5, relief="sunken", padding = 10)
-#gamesFrame.grid(column=0, row=0)
-
-#label4 = ttk.Label(gamesFrame, text="Fallout 4")
-#label4.grid(column=0, row = 0) 
-
-#label5 = ttk.Label(gamesFrame, text="Nier: Automata")
-#label5.grid(column=0, row = 1) 
-
 btnDelete = ttk.Button(listFrame, text='Delete', command=deleteGame)
 btnDelete.grid(column=0, row=1)
+
+messagebox.showinfo("", getMac())
 
 root.mainloop()
